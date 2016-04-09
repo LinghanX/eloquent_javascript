a1c6a4ad6d49503ed204b981a2676855dca48d06
LinghanX <linghan@me.com>
2016-04-09 17:33:33 -0400

    refactor chess_board

--- chess_board.js
+++ chess_board.js
@@ -1,26 +1,13 @@
 var size = 8;
 var string = '';
 for (var ver = 0; ver < size; ver++){
-  if( ver % 2 === 0 ) {
-    for (var hor = 0; hor < size; hor++) {
-      if(hor % 2 === 0) {
-        string = string + '#';
-      } else {
-        string = string + ' ';
-      }
+  for (var hor = 0; hor < size; hor++) {
+    if((hor + ver) % 2 === 0) {
+      string += '#';
+    } else {
+      string += ' ';
     }
-    string = string + "\n";
-  } else {
-    if( ver % 2 !== 0 ) {
-      for (var hor = 0; hor < size; hor++) {
-        if(hor % 2 !== 0) {
-          string = string + '#';
-        } else {
-          string = string + ' ';
-        }
-      }
-    }
-    string = string + "\n";
   }
+  string += "\n";
 }
 console.log(string);
